d63db9a5d2fe0ddd21246c99ddbb41b59a3ba77a
Brandon Whisnant <brandonwhisnant@Brandons-MacBook-Pro.local>
2019-09-05 23:16:28 -0400

    first commit

--- src/components/CharacterContainer.vue
+++ src/components/CharacterContainer.vue
@@ -1,19 +1,22 @@
 <template>
-    <b-container>
-      <b-row>
-        <Character
-          v-for="character in characters"
-          :key="character.id"
-          :img="character.picture"
-          :name="character.name"
-          :origin="character.origin"
-          :job="character.job"
-          :gender="character.gender"
-          :age="character.age"
-          :picture="character.picture"
-        />
-      </b-row>
-    </b-container>
+  <b-container>
+    <b-row class="ml-auto mr-auto text-center">
+      <h3>Final Fantasy Characters</h3>
+    </b-row>
+    <b-row>
+      <Character
+        v-for="character in characters"
+        :key="character.id"
+        :img="character.picture"
+        :name="character.name"
+        :origin="character.origin"
+        :job="character.job"
+        :gender="character.gender"
+        :age="character.age"
+        :picture="character.picture"
+      />
+    </b-row>
+  </b-container>
 </template>
 
 <script>
