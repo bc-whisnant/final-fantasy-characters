d4f224c062d8f3dd1c8dad9995f9fb20e4241dbf
Brandon Whisnant <brandonwhisnant@Brandons-MacBook-Pro.local>
2019-09-05 23:00:19 -0400

    first commit

---
+++ src/components/Character.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-col class="character-container" cols="4">
+      <b-card>
+        <img class="ff-image" :src="picture" alt>
+        <h3>{{ name }}</h3>
+        <h4>{{ origin }}</h4>
+        <h4>{{ job }}</h4>
+        <h4>{{ gender }}</h4>
+        <h4>{{ age }}</h4>
+      </b-card>
+    </b-col>
+</template>
+
+<script>
+export default {
+  name: "Character",
+  props: {
+    name: String,
+    origin: String,
+    job: String,
+    gender: String,
+    age: String,
+    picture: String
+  }
+};
+</script>
+
+<style scoped>
+.character-container {
+  margin-bottom: 2%;
+  text-align: center;
+}
+.ff-image {
+  width: 200px;
+  height: 300px;
+}
+</style>
---
+++ src/components/CharacterContainer.vue
@@ -0,0 +1,46 @@
+<template>
+    <b-container>
+      <b-row>
+        <Character
+          v-for="character in characters"
+          :key="character.id"
+          :img="character.picture"
+          :name="character.name"
+          :origin="character.origin"
+          :job="character.job"
+          :gender="character.gender"
+          :age="character.age"
+          :picture="character.picture"
+        />
+      </b-row>
+    </b-container>
+</template>
+
+<script>
+import Character from "./Character.vue";
+import axios from "axios";
+export default {
+  name: "CharacterContainer",
+  components: {
+    Character
+  },
+  data() {
+    return {
+      characters: []
+    };
+  },
+  mounted() {
+    this.getCharacters();
+  },
+  methods: {
+    getCharacters() {
+      axios
+        .get("https://www.moogleapi.com/api/v1/characters")
+        .then(response => {
+          this.characters = response.data;
+          console.log(this.characters);
+        });
+    }
+  }
+};
+</script>
